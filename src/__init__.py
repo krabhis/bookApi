86854468ba7a0500cb3483dfa0decb79a3ac115a
krabhis <abhishekkr212402@gmail.com>
2026-02-17 03:09:19 +0530

    added code

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,38 @@
-# Inside main.py title
+# # Inside main.py title
+# from fastapi import FastAPI
+# from src.books.routes import book_router
+
+# version = 'v1'
+
+# app = FastAPI(
+#     title='Bookly',
+#     description='A RESTful API for a book review web service',
+#     version=version,
+# )
+
+# app.include_router(book_router,prefix=f"/api/{version}/books", tags=['books'])  
+
+
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
 
-version = 'v1'
+
+#the lifespan event
+@asynccontextmanager
+async def lifespan(app: FastAPI):    
+    print("Server is starting...")
+    yield
+    print("server is stopping")
+
+
 
 app = FastAPI(
-    title='Bookly',
-    description='A RESTful API for a book review web service',
-    version=version,
+    lifespan=lifespan # add the lifespan event to our application
 )
 
-app.include_router(book_router,prefix=f"/api/{version}/books", tags=['books'])  
+app.include_router(
+    book_router,
+    prefix="/books",
+    tags=['books']
+)
