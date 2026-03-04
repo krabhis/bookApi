ced524951557ed5b6c25935527755f160f1d062c
krabhis <abhishekkr212402@gmail.com>
2026-03-04 23:05:11 +0530

    added services for auth

---
+++ src/auth/schemas.py
@@ -0,0 +1,6 @@
+class UserCreateModel(BaseModel):
+    first_name: str =Field(max_length=25)
+    last_name:  str =Field(max_length=25)
+    username: str = Field(max_length=8)
+    email: str = Field(max_length=40)
+    password: str  = Field(min_length=6)
